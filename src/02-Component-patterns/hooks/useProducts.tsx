c279e7571e925f9372c0154c8d63784319d0ae3b
Julian <julian.12040@gmail.com>
2022-04-28 22:43:41 -0500

    Control Props alt

--- src/02-Component-patterns/hooks/useProducts.tsx
+++ src/02-Component-patterns/hooks/useProducts.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { OnChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductsArgs {
@@ -11,13 +11,8 @@ const useProducts = ({onChange, product, value = 0}:useProductsArgs) => {
 
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef(!!onChange) 
-
     const increaseBy = (value:number) => {
 
-        if(isControlled.current){
-            return onChange!({count: value, product})
-        }
 
         const newValue = Math.max(counter + value,0)
 
--- src/02-Component-patterns/hooks/useShoppingCart.ts
+++ src/02-Component-patterns/hooks/useShoppingCart.ts
@@ -12,37 +12,20 @@ export const useShoppingCart = () => {
         // console.log('onProductCountChange', count, product);
         setShoppingCart((oldShoppingCart:any) => {
 
-            const productInCart: productInCart = oldShoppingCart[product.id] || {...product, count: 0};
+            if(count === 0){
 
-            if(Math.max(productInCart.count + count, 0) > 0){
-                productInCart.count += count
-                return {
-                    ...oldShoppingCart,
-                    [product.id]:productInCart
-                }
-            }
-
-            // borrar el producto
+                //eliminar un objeto de un arreglo usando desestructuracion
                 const { [product.id]:toDelete, ...rest } = oldShoppingCart;
 
                 return {
                     ...rest
                 }
+            } 
 
-            // if(count === 0){
-
-            //     //eliminar un objeto de un arreglo usando desestructuracion
-            //     const { [product.id]:toDelete, ...rest } = oldShoppingCart;
-
-            //     return {
-            //         ...rest
-            //     }
-            // } 
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: {...product, count}
-            // }
+            return {
+                ...oldShoppingCart,
+                [product.id]: {...product, count}
+            }
         })
 
     }
